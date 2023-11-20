66408c28c4912cee5d98c65a83923c35e545a5b9
Said Boufares <134112725+sabouf1@users.noreply.github.com>
2023-11-20 14:44:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('<p>This is Inass Talking to me but I don t understand what she is talking about!</p>'));
+app.get('/', (req, res) => res.send('<p>Hello World!</p>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
